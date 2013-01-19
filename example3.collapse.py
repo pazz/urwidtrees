7d9ad3cf6f30b3bcb2cca7798612079380e02cdb
Patrick Totzke <patricktotzke@gmail.com>
2013-01-19 22:18:45 +0000

    add filesys example and clean up

--- example3.collapse.py
+++ example3.collapse.py
@@ -7,32 +7,32 @@ from decoration import CollapsibleIndentedTree # for Decoration
 from widgets import TreeBox
 import urwid
 
-#if __name__ == "__main__":
-# Use (subclasses of) the wrapper decoration.CollapsibleTree to construct a
-# tree where collapsible subtrees. Apart from the original tree, these take a
-# callable `is_collapsed` that defines initial collapsed-status if a given
-# position.
+if __name__ == "__main__":
+    # Use (subclasses of) the wrapper decoration.CollapsibleTree to construct a
+    # tree where collapsible subtrees. Apart from the original tree, these take a
+    # callable `is_collapsed` that defines initial collapsed-status if a given
+    # position.
 
-# We want all grandchildren collapsed initially
-if_grandchild = lambda pos: stree.depth(pos) > 1
+    # We want all grandchildren collapsed initially
+    if_grandchild = lambda pos: stree.depth(pos) > 1
 
-# We use CollapsibleIndentedTree around the original example tree `stree`.
-# This uses Indentation to indicate the tree structure and squeezes in
-# text-icons to indicate the collapsed status.
-# Also try CollapsibleTree or CollapsibleArrowTree..
-tree = CollapsibleIndentedTree(stree,
-                               is_collapsed=if_grandchild,
-                               icon_focussed_att='focus',
-                               # indent=6,
-                               # childbar_offset=1,
-                               # icon_frame_left_char=None,
-                               # icon_frame_right_char=None,
-                               # icon_expanded_char='-',
-                               # icon_collapsed_char='+',
-                               )
+    # We use CollapsibleIndentedTree around the original example tree `stree`.
+    # This uses Indentation to indicate the tree structure and squeezes in
+    # text-icons to indicate the collapsed status.
+    # Also try CollapsibleTree or CollapsibleArrowTree..
+    tree = CollapsibleIndentedTree(stree,
+                                   is_collapsed=if_grandchild,
+                                   icon_focussed_att='focus',
+                                   # indent=6,
+                                   # childbar_offset=1,
+                                   # icon_frame_left_char=None,
+                                   # icon_frame_right_char=None,
+                                   # icon_expanded_char='-',
+                                   # icon_collapsed_char='+',
+                                   )
 
-# put the tree into a treebox
-treebox = TreeBox(tree)
+    # put the tree into a treebox
+    treebox = TreeBox(tree)
 
-rootwidget = urwid.AttrMap(treebox, 'body')
-urwid.MainLoop(rootwidget, palette).run()  # go
+    rootwidget = urwid.AttrMap(treebox, 'body')
+    urwid.MainLoop(rootwidget, palette).run()  # go
