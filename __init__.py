93fdf413c407a6fd100917751d535a8cbf21f5ae
Patrick Totzke <patricktotzke@gmail.com>
2013-02-21 15:33:07 +0000

    fix import errors

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ except:
 
 from tree import Tree, SimpleTree
 from decoration import DecoratedTree, CollapsibleTree
-from decoration import IndentedTree, CollapsibleIndentedTree,
-frm decoration import ArrowTree, CollapsibleArrowTree
+from decoration import IndentedTree, CollapsibleIndentedTree
+from decoration import ArrowTree, CollapsibleArrowTree
 from nested import NestedTree
 from widgets import TreeBox
