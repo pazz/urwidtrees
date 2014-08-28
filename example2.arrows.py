8847ae99a7ffc77c0690a543037b265e84b1b1ae
AdotDdot <adotddot1123@gmail.com>
2014-08-28 10:29:54 +0000

    Update example2.arrows.py

--- example2.arrows.py
+++ example2.arrows.py
@@ -20,8 +20,14 @@ if __name__ == "__main__":
                       # arrow_connector_lchar=u'\u255A',
                       )
 
+def unhandled_input(k):
+    #exit on q
+    if k in ['q', 'Q']: raise urwid.ExitMainLoop()
+
     # put the into a treebox
     treebox = TreeBox(atree)
-
     rootwidget = urwid.AttrMap(treebox, 'body')
-    urwid.MainLoop(rootwidget, palette).run()  # go
+    #add a text footer
+    footer = urwid.AttrMap(urwid.Text('Q to quit'), 'focus')
+    #enclose in a frame
+    urwid.MainLoop(urwid.Frame(rootwidget, footer=footer), palette, unhandled_input = unhandled_input).run()  # go
