5b9c8a0faaa19515aadaddfa427541e3aeff9738
AdotDdot <adotddot1123@gmail.com>
2014-08-28 10:34:44 +0000

    Update example4.filesystem.py

--- example4.filesystem.py
+++ example4.filesystem.py
@@ -104,6 +104,10 @@ class DirectoryTree(Tree):
             candidate = siblings[myindex - 1]
         return candidate
 
+def unhandled_input(k):
+    #exit on q
+    if k in ['q', 'Q']: raise urwid.ExitMainLoop()
+
 if __name__ == "__main__":
     cwd = os.getcwd()  # get current working directory
     dtree = DirectoryTree()  # get a directory walker
@@ -124,4 +128,7 @@ if __name__ == "__main__":
     # stick it into a TreeBox and use 'body' color attribute for gaps
     tb = TreeBox(decorated_tree, focus=cwd)
     root_widget = urwid.AttrMap(tb, 'body')
-    urwid.MainLoop(root_widget, palette).run()  # go
+    #add a text footer
+    footer = urwid.AttrMap(urwid.Text('Q to quit'), 'focus')
+    #enclose all in a frame
+    urwid.MainLoop(urwid.Frame(rootwidget, footer=footer), palette, unhandled_input = unhandled_input).run() # go
