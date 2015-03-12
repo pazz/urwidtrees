119a53112006b48f70136446870d08971d89f50c
Werner Beroux <werner@beroux.com>
2015-03-12 17:01:55 +0100

    Update example6.append.py
    Fix

--- docs/examples/example6.append.py
+++ docs/examples/example6.append.py
@@ -4,14 +4,15 @@ Update the tree after it's initially build.
 Shows something like:
 
 root                                                                                                                            
-├─➤PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.                                                                             
-│  64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.039 ms                                                                     
-│                                                                                                                               
-├─➤64 bytes from 127.0.0.1: icmp_seq=2 ttl=64 time=0.053 ms                                                                     
-│                                                                                                                               
-└─➤64 bytes from 127.0.0.1: icmp_seq=3 ttl=64 time=0.064 ms                                                                     
+|->PING 127.0.0.1 (127.0.0.1) 56(84) bytes of data.                                                                             
+|  64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.039 ms                                                                     
+|                                                                                                                               
+|->64 bytes from 127.0.0.1: icmp_seq=2 ttl=64 time=0.053 ms                                                                     
+|                                                                                                                               
+|->64 bytes from 127.0.0.1: icmp_seq=3 ttl=64 time=0.064 ms                                                                     
 
 """
+import subprocess
 import urwid
 import urwidtrees
 
