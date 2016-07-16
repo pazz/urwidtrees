9629ddfcf65f54499eab36dd0129b96734d7f463
Mathias Stelzer <knoppo@rolln.de>
2016-07-16 14:40:03 +0200

    move package meta-info to setup.py

--- urwidtrees/__init__.py
+++ urwidtrees/__init__.py
@@ -1,12 +1,3 @@
-__productname__ = 'urwidtrees'
-__version__ = '1.0.2'
-__copyright__ = "Copyright (C) 2015 Patrick Totzke"
-__author__ = "Patrick Totzke"
-__author_email__ = "patricktotzke@gmail.com"
-__description__ = "Tree widgets for urwid"
-__url__ = "https://github.com/pazz/urwidtrees"
-__license__ = "Licensed under the GNU GPL v3+."
-
 from .tree import Tree, SimpleTree
 from .decoration import DecoratedTree, CollapsibleTree
 from .decoration import IndentedTree, CollapsibleIndentedTree
