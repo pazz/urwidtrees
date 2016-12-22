pazz/urwidtrees

commit 9629ddfcf65f54499eab36dd0129b96734d7f463

move package meta-info to setup.py

setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
-import urwidtrees
 
 
-setup(name='urwidtrees',
-      version=urwidtrees.__version__,
-      description=urwidtrees.__description__,
-      author=urwidtrees.__author__,
-      author_email=urwidtrees.__author_email__,
-      url=urwidtrees.__url__,
-      license=urwidtrees.__copyright__,
-      packages=['urwidtrees'],
-      requires=['urwid (>=1.1.0)'],
-     )
+__productname__ = 'urwidtrees'
+__version__ = '1.0.2'
+__copyright__ = "Copyright (C) 2015 Patrick Totzke"
+__author__ = "Patrick Totzke"
+__author_email__ = "patricktotzke@gmail.com"
+__description__ = "Tree widgets for urwid"
+__url__ = "https://github.com/pazz/urwidtrees"
+__license__ = "Licensed under the GNU GPL v3+."
+
+
+setup(
+    name='urwidtrees',
+    version=__version__,
+    description=__description__,
+    author=__author__,
+    author_email=__author_email__,
+    url=__url__,
+    license=__copyright__,
+    packages=['urwidtrees'],
+    install_requires=['urwid>=1.1.0'],
+)

urwidtrees/__init__.py
@@ -1,12 +1,3 @@
-__productname__ = 'urwidtrees'
-__version__ = '1.0.2'
-__copyright__ = "Copyright (C) 2015 Patrick Totzke"
-__author__ = "Patrick Totzke"
-__author_email__ = "patricktotzke@gmail.com"
-__description__ = "Tree widgets for urwid"
-__url__ = "https://github.com/pazz/urwidtrees"
-__license__ = "Licensed under the GNU GPL v3+."
-
 from .tree import Tree, SimpleTree
 from .decoration import DecoratedTree, CollapsibleTree
 from .decoration import IndentedTree, CollapsibleIndentedTree

commit 4c0e88745e651c08998a13200258d103d26a5843

move author/version info back to __init__.py
for accessibility in the docs. This fixes docs-builds on rtfd.org which
was broken since 9629ddfcf65

docs/source/conf.py
@@ -40,7 +40,7 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(os.path.join('..','..')))
-from urwidtrees import __version__,__author__
+from urwidtrees import __version__,__copyright__
 
 # -- General configuration ------------------------------------------------
 
@@ -72,7 +72,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'urwidtrees'
-copyright = u'2015, Patrick Totzke'
+copyright = __copyright__
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

setup.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-
-__productname__ = 'urwidtrees'
-__version__ = '1.0.2'
-__copyright__ = "Copyright (C) 2015 Patrick Totzke"
-__author__ = "Patrick Totzke"
-__author_email__ = "patricktotzke@gmail.com"
-__description__ = "Tree widgets for urwid"
-__url__ = "https://github.com/pazz/urwidtrees"
-__license__ = "Licensed under the GNU GPL v3+."
-
+import urwidtrees
 
 setup(
     name='urwidtrees',
-    version=__version__,
-    description=__description__,
-    author=__author__,
-    author_email=__author_email__,
-    url=__url__,
-    license=__copyright__,
+    version=urwidtrees.__version__,
+    description="Tree widgets for urwid",
+    author="Patrick Totzke",
+    author_email="patricktotzke@gmail.com",
+    url="https://github.com/pazz/urwidtrees",
+    license="Licensed under the GNU GPL v3+.",
     packages=['urwidtrees'],
     install_requires=['urwid>=1.1.0', 'mock'],
 )

urwidtrees/__init__.py
@@ -1,3 +1,7 @@
+__version__ = '1.0.3.dev'
+__copyright__ = "Copyright (C) 2016 Patrick Totzke"
+__license__ = "Licensed under the GNU GPL v3+."
+
 from .tree import Tree, SimpleTree
 from .decoration import DecoratedTree, CollapsibleTree
 from .decoration import IndentedTree, CollapsibleIndentedTree